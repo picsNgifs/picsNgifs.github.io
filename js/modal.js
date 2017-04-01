0d4d81384106b79d05fe859c22f0fe8b56b06fb9
aldmarj <aldmar.joubert@students.plymouth.ac.uk>
2017-04-01 22:30:50 +0100

    initial

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('.pop').on('click', function () {
+        var title = $(".modal-title");
+
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        title.html($(this).find("img").attr("alt"));
+        $('#myModal').modal('show');
+
+    });
+});
